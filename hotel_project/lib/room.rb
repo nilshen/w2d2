9db674f45459fd889eefa1af7c2686a7d1a868a7
nilshen <linshen88@gmail.com>
2022-04-05 15:24:20 -0400

    room class

--- hotel_project/lib/room.rb
+++ hotel_project/lib/room.rb
@@ -1,3 +1,27 @@
 class Room
-  
+  attr_reader :capacity, :occupants
+
+  def initialize(num)
+    @capacity = 3
+    @occupants = []
+  end
+
+  def full?
+    if occupants.length < 3
+        return false
+    end
+    true
+  end
+
+  def available_space
+    @capacity - occupants.length
+  end
+
+  def add_occupant (str)
+    if !full?
+        occupants << str 
+        return true
+    end
+    false
+  end
 end
