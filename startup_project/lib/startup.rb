e295704fe867bb5ada2095625d3eaa1e8d50f64e
nilshen <linshen88@gmail.com>
2022-04-05 21:34:29 -0400

    w2d2 night owl

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -1,5 +1,54 @@
 require "employee"
 
 class Startup
+    attr_reader(:name, :funding,:salaries, :employees)
+    def initialize(name, funding, salaries)
+    @name = name
+    @funding = funding
+    @salaries = salaries
+    @employees = []
+    end
 
+    def valid_title?(title)
+        if salaries.has_key?(title)
+            return true
+        end 
+    false
+    end 
+            
+    def >(other_starup)
+        if self.funding > other_starup.funding
+            return true
+        end
+    false
+    end
+
+    def hire (employee_name, title)
+        if self.salaries.has_key?(title)
+        employee_name = Employee.new(employee_name, title)
+        employees << employee_name
+        else raise "Who's this?"
+        end
+    end 
+
+    def size 
+        return employees.length
+    end
+
+    def pay_employee(employee)
+        money_to_pay = @salaries[employee.title]
+        if @funding >= money_to_pay
+            employee.pay(money_to_pay)
+            @funding -= money_to_pay
+        else raise "no money!!!!!"
+        end
+    end 
+
+    def payday
+        @employees.each do |employee|
+            pay_employee(employee)
+        end
+    end
 end
+
+
